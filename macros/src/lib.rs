6ada8ca890eda64e5e9bd12d2ed9334388c969f4
Joel Jacobson <joel@trustly.com>
2017-01-08 07:33:43 -1000

    Fix broken build due to nightly refactoring. Use syntax::symbol::Symbol instead of old syntax::parse::token::intern.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -8,7 +8,7 @@ extern crate rustc_plugin;
 
 use rustc_plugin::Registry;
 use syntax::ext::base::{MultiDecorator, MultiModifier};
-use syntax::parse::token::intern;
+use syntax::symbol::Symbol;
 use rustc::hir::map::blocks::MaybeFnLike;
 
 use syntax::ext::base::{ExtCtxt, Annotatable};
@@ -18,11 +18,10 @@ use syntax::ptr::P;
 use syntax::ast::{Item, MetaItem};
 use syntax::attr;
 use syntax_ext::deriving::generic::{combine_substructure, EnumMatching, FieldInfo, MethodDef, Struct, Substructure, TraitDef, ty};
-use syntax::parse::token::InternedString;
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_syntax_extension(intern("pg_export"), MultiModifier(box expand_pg_export));
+    reg.register_syntax_extension(Symbol::intern("pg_export"), MultiModifier(box expand_pg_export));
 }
 
 pub fn expand_pg_export(cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: Annotatable) -> Annotatable {
@@ -35,17 +34,17 @@ pub fn expand_pg_export(cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: Annota
     match item {
         Annotatable::Item(it) => {
             let mut new_it = (*it).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(Symbol::intern("no_mangle"))));
             Annotatable::Item(P(new_it))
         }
         Annotatable::ImplItem(it) => {
             let mut new_it = (*it).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(Symbol::intern("no_mangle"))));
             Annotatable::ImplItem(P(new_it))
         }
         Annotatable::TraitItem(tt) => {
             let mut new_it = (*tt).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(Symbol::intern("no_mangle"))));
             Annotatable::TraitItem(P(new_it))
         }
     }
